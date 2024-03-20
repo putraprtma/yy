44795f18898c88f902cd33805b02574fb8bd6813
putraprtma <putrapratamagntg@gmail.com>
2024-03-20 21:35:15 +0700

    neng sekolahan

--- tambah.php
+++ tambah.php
@@ -1,66 +1,62 @@
 <!DOCTYPE html>
 <html>
 <style>
-input[type=text],input[type=number], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+  input[type=text],
+  input[type=number],
+  select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    box-sizing: border-box;
+  }
 
-input[type=submit] {
-  width: 100%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
+  input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
 
-input[type=submit]:hover {
-  background-color: #45a049;
-}
+  input[type=submit]:hover {
+    background-color: #45a049;
+  }
 
-div {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
+  div {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+  }
 </style>
+
 <body>
 
-<h3>Using CSS to style an HTML Form</h3>
+  <h3>Using CSS to style an HTML Form</h3>
 
-<div>
-  <form action="tambahaksi.php" method="post">
+  <div>
+    <form action="tambahaksi.php" method="post">
 
-    <label for="fnik">nik</label>
-    <input type="input" id="fnik" name="fnik" placeholder="Isikan nama barang..">
+      <label for="fnik">nik</label>
+      <input type="input" id="fnik" name="fnik" placeholder="Isikan nama barang..">
 
-    <label for="fberat">berat</label>
-    <input type="number" id="fberat" name="fberat" placeholder="isikan berat barang..">
+      <label for="fberat">berat</label>
+      <input type="number" id="fberat" name="fberat" placeholder="isikan berat barang..">
 
-     <label for="fharga">harga</label>
-    <input type="number" id="fharga" name="fharga" placeholder="isikan harga barang..">
+      <label for="fharga">harga</label>
+      <input type="number" id="fharga" name="fharga" placeholder="isikan harga barang..">
 
-    
-  
-    <input type="submit" value="Submit">
-  </form>
-</div>
+
+
+      <input type="submit" value="Submit">
+    </form>
+  </div>
 
 </body>
+
 </html>
-
-
-
-
-
-    
-  
-    
